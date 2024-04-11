772acb54a3c8176ccd56deb9026e6aa5a6d8a19f
Aryan Gupta <guptaaryan926@gmail.com>
2024-04-12 03:08:14 +0530

    initial commit

---
+++ client.py
@@ -0,0 +1,8 @@
+#Import all of joel's code
+
+#Call function with parameter: Json output file path
+
+#Response is a List of 2 Dictionaries
+
+#This response will call be sent as a parameter to the machinme learning model
+
